bc0207be03bebfead24782067b18d31d67e59938
nainshi <nainshichaursiya@92gmail.com>
2023-09-14 23:50:06 +0530

    ex7,2.py

---
+++ loops/ex7.py
@@ -0,0 +1,19 @@
+from turtle import *
+#make a turtle object
+
+speed('slowest')
+pensize(2)
+pencolor('black')
+bgcolor('light yellow')
+left(50)
+fd(120)
+fillcolor('red')
+begin_fill()
+circle(45,200)
+lt(220)
+circle(45,200)
+fd(120)
+
+end_fill()
+hideturtle()
+mainloop()
