854632af5e8be4c387886ba78b3658098fa8730b
nainshi <nainshichaurasiya92@gmail.com>
2023-09-18 00:22:53 +0530

    ex5,8.0

--- loops/ex3.py
+++ loops/ex3.py
@@ -1,12 +1,36 @@
 from turtle import *
-speed('slowest')
-dis = [100,100,100,100,100,100,70,60,70]
+speed('fastest')
+pensize(4)
+dis = [100,100,100,100,100,100,70,60,70,]
 ng1 = [90,45,90,45,90,90,90,90,90]
 
 for d,a in zip(dis,ng1):
     fd(d)
     lt(a)
+penup()    
+goto(24,90)
+pendown()
+circle(20,360) 
+penup()
+fd(77)
+lt(90)
+fd(10)
+pendown() 
+
+l1=[120,100,17]
+l2=[45,90,90]
+for i,j in zip(l1,l2):
+    lt(j)
+    fd(i)
+penup()
+
+rt(90)
+fd(4)
+lt(47.9)
+pendown()
+
+fd(100)    
 
     
-hideturtle()
+#hideturtle()
 mainloop()
