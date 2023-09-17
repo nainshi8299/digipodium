91c493759575620f49b1c1e45deaf83af0af4d32
nainshi <nainshichaurasiya92@gmail.com>
2023-09-17 23:27:14 +0530

    ex1,4.0

--- loops/ex1.py
+++ loops/ex1.py
@@ -5,12 +5,16 @@ from turtle import *
 speed('fastest')
 bgcolor('white')
 pencolor('red')
+fillcolor('green')
+begin_fill()
 for i in range(6):
     fd(100)
     lt(360/6)
+    
     #write(i)
     #circle(i)
-       
+    
+end_fill()   
 
 hideturtle()
 mainloop()    
