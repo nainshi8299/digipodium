599e4773c87d812f7392842b4bc299316e366930
nainshi <nainshichaurasiya92@gmail.com>
2024-08-23 16:10:01 +0530

    new

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print('hello world')
+#print('hello world')
+import calendar
+
+def generate_calendar(year, month):
+    cal = calendar.monthcalendar(year, month)
+    print("  S  M  T  W  T  F  S")
+    for week in cal:
+        for day in week:
+            if day == 0:
+                print(" may   ", end="")
+            else:
+                print(f"{day:2} ", end="")
+        print()
+
+if __name__ == "__main__":
+    year = int(input("Enter year: "))
+    month = int(input("Enter month: "))
+    generate_calendar(year, month)
